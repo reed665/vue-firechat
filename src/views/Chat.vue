adccdbeaadf9045f96e5de279327c82e0749ca34
Alexander <reed665@hotmail.com>
2018-05-02 13:45:02 +0300

    apply some styles to chat messages list

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -69,4 +69,21 @@ h2 {
   display: block;
   font-size: .8em;
 }
+
+.message-list {
+  max-height: 300px;
+  overflow: auto;
+}
+.message-list::-webkit-scrollbar {
+  width: 5px;
+}
+.message-list::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+.message-list::-webkit-scrollbar-thumb {
+  background: #888; 
+}
+.message-list::-webkit-scrollbar-thumb:hover {
+  background: #555; 
+}
 </style>
