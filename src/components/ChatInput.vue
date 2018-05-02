8cd8c29c37161b8a30b98572d973fe38fd393870
Alexander <reed665@hotmail.com>
2018-05-02 12:59:57 +0300

    update ChatInput component

--- src/components/ChatInput.vue
+++ src/components/ChatInput.vue
@@ -3,24 +3,40 @@
     <form @submit.prevent="addMessage">
       <label for="new-message">New Message (enter to add)</label>
       <input type="text" name="new-message" v-model="newMessage">
+      <p class="red-text" v-if="feedback">{{ feedback }}</p>
     </form>
   </div>
 </template>
 
+
 <script>
+import db from '@/firebase'
+
 export default {
   props: ['name'],
   data () {
     return {
       newMessage: '',
+      feedback: '',
     }
   },
   methods: {
     addMessage () {
       if (!this.newMessage) {
+        this.feedback = 'You must enter a message in order to send one'
         return
       }
-      console.log(this.newMessage, this.name, Date.now())
+      this.feedback = ''
+      const doc = {
+        content: this.newMessage,
+        name: this.name,
+        timestamp: Date.now()
+      }
+      db.collection('messages').add(doc)
+        .then(() => {
+          this.newMessage = ''
+        })
+        .catch(console.error)
     }
   }
 }
