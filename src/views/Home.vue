5f4e585248ce40bb3a8f7fa1255323ca0a6d60f3
Alexander <reed665@hotmail.com>
2018-05-02 10:10:04 +0300

    update home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="teal-text">Welcome</h2>
+        <form @submit.prevent="enterChat">
+          <label for="name">Enter your name</label>
+          <input type="text" name="name" v-model="name">
+          <button class="btn teal">Enter Chat</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      name: null,
+    }
+  },
+  methods: {
+    enterChat () {
+      console.log(this.name)
+    }
   }
 }
 </script>
+
+<style scoped>
+  .container {
+    margin-top: 100px;
+    max-width: 400px;
+  }
+  h2 {
+    font-size: 3em;
+  }
+  button {
+    margin-top: 30px;
+    margin-bottom: 30px;
+  }
+</style>
